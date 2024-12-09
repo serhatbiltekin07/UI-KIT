536224ad844a980e0a4e447c5d91631165c90891
serhatbiltekin07 <biltekin_kurtulus@hotmail.com>
2024-12-09 16:57:09 +0300

    img

--- UI-KIT/src/Pages/Home/Home.jsx
+++ UI-KIT/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Genre from '../../components/Genre/Genre'
+import './Home.css'
 const Home = () => {
   return (
     <div className='home'>
